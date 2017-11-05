470eec4d0c2c31924f393820641a83fecb294382
Chernov Evgeniy <ev.br.ch@gmail.com>
2017-11-05 20:42:45 +0300

    telebot: refactoring code

--- telegram/bot/bot_t.py
+++ telegram/bot/bot_t.py
@@ -2,12 +2,12 @@
 import telebot
 import config
 import request
+
 # Создаем объект бота
 bot = telebot.TeleBot(config.token)
 # Инициализируем пустые списки локаций и команд метеостанций
 LMeteoLocation = []
 LMeteoCommand = []
-
 
 # Функция определяющяя списки локаций и команд метеостанций
 def listMeteoLocation(LMeteoLocation, LMeteoCommand):
@@ -17,82 +17,79 @@ def listMeteoLocation(LMeteoLocation, LMeteoCommand):
                               ' ' + str(meteoName[i][1]))
         LMeteoCommand.append('Meteo' + str(meteoName[i][0]))
 
+def run_bot():
+        # Описание возвращаемых параметров
+        paramMeteoLoc = ['-Состояние дорожного полотна: ', '-Влажность: ',
+                         '-Атмосферное давление: ', '-Дата: ',
+                         '-Предупреждения об опастности: ', '-Температура воздуха: ']
+        # Описание обозначений возвращаемых параметров
+        paramMeteoLocDes = ['', ' %', ' mm Hg', '', '', ' °C']
+        # Название параметров возвращаемыъх с сервиса
+        paramMeteoLocGet = ['surfacecondition', 'humidity', 'pressure', 'date',
+                            'surfacewarning', 'airTemperature']
+        # Словарь соответствич прогноза
+        dictForeCast = {
+             'clear-day': 'Ясно',
+             'clear-night': 'Ясно',
+             'rain': 'Ожидается дождь',
+             'snow': 'Ожидается снег',
+             'sleet': 'Ожидается дождь со снегом',
+             'wind': 'Ожидается ветренная погода',
+             'fog': 'Ожидается туман',
+             'cloudy': 'Облачно',
+             'partly-cloudy-day': 'Переменная облачность',
+             'partly-cloudy-night': 'Переменная облачность',
+             'hail': 'Ожидается град',
+             'thunderstorm': 'Ожидается гроза',
+             'tornado': 'ТОРНАДО!',
+             'err': 'Ошибка! Прогноз временно отсутствует'
+        }
 
-# Описание возвращаемых параметров
-paramMeteoLoc = ['-Состояние дорожного полотна: ', '-Влажность: ',
-                 '-Атмосферное давление: ', '-Дата: ',
-                 '-Предупреждения об опастности: ', '-Температура воздуха: ']
-# Описание обозначений возвращаемых параметров
-paramMeteoLocDes = ['', ' %', ' mm Hg', '', '', ' °C']
-# Название параметров возвращаемыъх с сервиса
-paramMeteoLocGet = ['surfacecondition', 'humidity', 'pressure', 'date',
-                    'surfacewarning', 'airTemperature']
-# Словарь соответствич прогноза
-dictForeCast = {
-     'clear-day': 'Ясно',
-     'clear-night': 'Ясно',
-     'rain': 'Ожидается дождь',
-     'snow': 'Ожидается снег',
-     'sleet': 'Ожидается дождь со снегом',
-     'wind': 'Ожидается ветренная погода',
-     'fog': 'Ожидается туман',
-     'cloudy': 'Облачно',
-     'partly-cloudy-day': 'Переменная облачность',
-     'partly-cloudy-night': 'Переменная облачность',
-     'hail': 'Ожидается град',
-     'thunderstorm': 'Ожидается гроза',
-     'tornado': 'ТОРНАДО!',
-     'err': 'Ошибка! Прогноз временно отсутствует'
-}
-# listMeteoLocation(LMeteoLocation, LMeteoCommand)
-print(LMeteoLocation, LMeteoCommand)
-# Получение команды /start или /help
+        #print(LMeteoLocation, LMeteoCommand)
+        # Получение команды /start или /help
+        '''Функция обработки команды /start или /help
+            с выводом списка метеостанций одним сообщением'''
+        @bot.message_handler(commands=['start', 'help'])
+        def start(message):
+            listMeteoPr = 'List meteostation in Belgorodskaya oblast \n'
+            for i in range(0, len(LMeteoLocation)):
+                listMeteoPr = listMeteoPr + '\n' + LMeteoLocation[i]
+            bot.send_message(message.chat.id, listMeteoPr)
 
-
-@bot.message_handler(commands=['start', 'help'])
-'''Функция обработки команды /start или /help
-   с выводом списка метеостанций одним сообщением'''
-
-
-def start(message):
-    listMeteoPr = 'List meteostation in Belgorodskaya oblast \n'
-    for i in range(0, len(LMeteoLocation)):
-        listMeteoPr = listMeteoPr + '\n' + LMeteoLocation[i]
-    bot.send_message(message.chat.id, listMeteoPr)
-
-'''Получение команды по одной из метеостанции
-   и вывод реальных данных'''
-
-
-@bot.message_handler(commands=LMeteoCommand[:])
-def meteo(message):
-    meteoState = request.DataPressure()
-    #Вывод названия метеостанции с её месторасположением и прогноз
-    for i in range(0, len(LMeteoCommand)):
-        if message.text[1:] == LMeteoCommand[i]:
-            botMess = LMeteoLocation[i][1:] + '\n'
-            break
-    # Формирование сообщения параметров метеостанции
-    if len(message.text) > 7:
-        masMeteo = meteoState[int(message.text[-2:])]
-        for i in range(0, len(paramMeteoLoc)):
-            if masMeteo[paramMeteoLocGet[i]] == 'null':
-                botMess = botMess + paramMeteoLoc[i] +
-                'Данные отсутствуют' + '\n'
+        '''Получение команды по одной из метеостанции
+           и вывод реальных данных'''
+        @bot.message_handler(commands=LMeteoCommand[:])
+        def meteo(message):
+            meteoState = request.DataPressure()
+            #Вывод названия метеостанции с её месторасположением и прогноз
+            for i in range(0, len(LMeteoCommand)):
+                if message.text[1:] == LMeteoCommand[i]:
+                    botMess = LMeteoLocation[i][1:] + '\n'
+                    break
+            # Формирование сообщения параметров метеостанции
+            if len(message.text) > 7:
+                masMeteo = meteoState[int(message.text[-2:])]
+                print(masMeteo)
+                print('-_- ---'*10)
+                for i in range(0, len(paramMeteoLoc)):
+                    if masMeteo[paramMeteoLocGet[i]] == 'null':
+                        botMess = botMess + paramMeteoLoc[i] + 'Данные отсутствуют' + '\n'
+                    else:
+                        botMess = botMess + paramMeteoLoc[i] + str(masMeteo[paramMeteoLocGet[i]]) + paramMeteoLocDes[i] + '\n'
             else:
-                botMess = botMess + paramMeteoLoc[i] +
-                masMeteo[paramMeteoLocGet[i]] + paramMeteoLocDes[i] + '\n'
-        #botMess = botMess + '-Прогноз: ' + dictForeCast[masMeteo['symbol']]
-        #bot.send_message(message.chat.id, botMess)
-    else:
-        masMeteo = meteoState[int(message.text[-1])]
-        for i in range(0, len(paramMeteoLoc)):
-            if masMeteo[paramMeteoLocGet[i]] == 'null':
-                botMess = botMess + paramMeteoLoc[i] +
-                'Данные отсутствуют' + '\n'
-            else:
-                botMess = botMess + paramMeteoLoc[i] +
-                masMeteo[paramMeteoLocGet[i]] + paramMeteoLocDes[i] + '\n'
-        #botMess = botMess + '-Прогноз: ' + dictForeCast[masMeteo['symbol']]
-    bot.send_message(message.chat.id, botMess)
-bot.polling()
+                masMeteo = meteoState[int(message.text[-1])]
+                for i in range(0, len(paramMeteoLoc)):
+                    if masMeteo[paramMeteoLocGet[i]] == 'null':
+                        botMess = botMess + paramMeteoLoc[i] + 'Данные отсутствуют' + '\n'
+                    else:
+                        botMess = botMess + paramMeteoLoc[i] + str(masMeteo[paramMeteoLocGet[i]]) + paramMeteoLocDes[i] + '\n'
+            bot.send_message(message.chat.id, botMess)
+        bot.polling()
+
+while True:
+    try:
+        listMeteoLocation(LMeteoLocation, LMeteoCommand)
+        run_bot()
+    except Exception:
+        print('tak blet')
+        continue
--- telegram/bot/request.py
+++ telegram/bot/request.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 import requests
+from datetime import datetime
 import time
-import threading
 import config
+import json
+
+#Класс метеостанции
+class Meteostations:
+    def __init__(self, iden, name, position):
+        self.iden = iden
+        self.name = name
+        self.position = position
 
 
-# Функиця определения является ли строка числом
-def is_num(sMeteoVal):
+#Функция выполнения запроса к сайту агрегатору
+def request_meteo(req_url):
     try:
-	    float(sMeteoVal)
-        return True
-    except ValueError:
-        return False
-
-
-# Запрос списка метеостанций и вывод в строчном значении
-def IdMeteoLst():
-    try:
-        reqGet = requests.get(config.adrrMapIndex, timeout=15)
+        reqGet = requests.get(req_url, timeout = 15)
         reqGet.raise_for_status()
     except requests.exceptions.ConnectTimeout:
         print('Error! Connect timeout ocurred!')
@@ -28,93 +27,91 @@ def IdMeteoLst():
     except requests.exceptions.HTTPError as err:
         print('Error! HTTP Errors occured!')
         print('Response is: {connect}'.format(content=err.response.content))
-    reqMeteo = reqGet.text
-    spReqMet = reqMeteo.split(',')
+    return reqGet
+
+#Функиця определения является ли строка числом
+def is_num(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+
+#Запрос списка метеостанций и вывод в строчном значении
+def IdMeteoLst():
+    reqGet = request_meteo(config.adrrMapIndex)
+    s_json = json.loads(reqGet.text)
     spListID = []
     spListNM = []
     spListFC = []
-    # Создаем список id метеостанций и названий
-    for i in range(len(spReqMet)):
-        sPointID = spReqMet[i].find('id')
-        sPointNM = spReqMet[i].find('name')
-        sPointFC = spReqMet[i].find('symbol')
-        if sPointID != -1:
-            spListID.append(int(spReqMet[i][spReqMet[i].rfind(':') + 1:]))
-        if sPointNM != -1:
-            spListNM.append(spReqMet[i][spReqMet[i].rfind(':') + 2:-1])
-        if sPointFC != -1:
-            if spReqMet[i][-1] == ']' and spReqMet[i][-2] == '}':
-                spListFC.append(spReqMet[i][spReqMet[i].rfind(':') + 2:-3])
-            elif spReqMet[i][-1] == '}' or ']':
-                spListFC.append(spReqMet[i][spReqMet[i].rfind(':') + 2:-2])
-            else:
-                spListFC.append(spReqMet[i][spReqMet[i].rfind(':') + 2:-1])
+    for i in range(len(s_json)):
+        spListID.append(s_json[i]['id'])
+        spListNM.append(s_json[i]['name'])
+        spListFC.append(s_json[i]['symbol'])
     return list(zip(spListID, spListNM, spListFC))
 
-
 def DictDataMeteo():
-    # Создаем список словарей данных с каждой метеостанции
+    #Создаем список словарей данных с каждой метеостанции
     lstIdNameFC = IdMeteoLst()
     lDictMeteo = []
     nMeteo = []
     dictMeteo = {}
     for j in range(len(lstIdNameFC)):
         nMeteo.append(lstIdNameFC[j][0])
-        try:
-            reqGet = requests.get(config.adrrMapLast +
-                                  str(nMeteo[j]), timeout=15)
-            reqGet.raise_for_status()
-        except requests.exceptions.ConnectTimeout:
-            print('Error! Connect timeout ocurred!')
+        map_index = config.adrrMapLast + str(nMeteo[j])
+        reqGet = request_meteo(map_index)
+        if reqGet == None:
             continue
-        except requests.exceptions.RealTimeout:
-            print('Error! Read timeout ocurred!')
-            continue
-        except requests.exceptions.ConnectionError:
-            print('Seems like dns lookup failed.')
-            continue
-        except requests.exceptions.HTTPError as err:
-            print('Error! HTTP Errors occured!')
-            print('Response is: {connect}'
-                  .format(content=err.response.content))
-            continue
-        splMeteo = reqGet.text.split(',')
-        x, y = splMeteo[0].split('":"')
-        dictMeteo = {}
-        dictMeteo[x[2:]] = y[:-1]
-        for i in range(1, len(splMeteo)):
-            x, y = splMeteo[i].split(':')
-            if is_num(y) or y == 'null':
-                dictMeteo[x[1:-1]] = y
-            else:
-                if i == len(splMeteo):
-                    dictMeteo[x[1:-1]] = y[1:-2]
-                else:
-                    dictMeteo[x[1:-1]] = y[1:-1]
-        dictMeteo['symbol'] = lstIdNameFC[j][2]
-        lDictMeteo.append(dictMeteo)
+        s_json = json.loads(reqGet.text)
+        s_json['date'] = str(datetime.strptime(s_json['date'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
+        lDictMeteo.append(s_json)
     return dict(zip(nMeteo, lDictMeteo))
 
 
 def DataPressure():
-    '''Преобразование даты в читаемый формат с
-       коррекцией и атмосферного давления в мм.рт.ст'''
+    #Преобразование даты в читаемый формат с коррекцией и атмосферного давления в мм.рт.ст
     lisDictMeteo = DictDataMeteo()
     for i in list(lisDictMeteo.keys()):
-        bufd, buft = lisDictMeteo[i]['date'].split('T')
+        bufd, buft = lisDictMeteo[i]['date'].split()
         y, m, d = bufd.split('-')
         bufd = d + '-' + m + '-' + y
         h, m, s = buft.split(':')
         h = int(h)
         h += 3
         buft = str(h) + ':' + m + ':' + s
-        lisDictMeteo[i]['date'] = bufd + ' ' + buft[:-5]
+        lisDictMeteo[i]['date'] = bufd + ' ' + buft
         try:
             bufPress = lisDictMeteo[i]['pressure']
             bufPress = float(bufPress)
             bufPress *= 0.750063
-            lisDictMeteo[i]['pressure'] = str(round(bufPress, 1))
+            lisDictMeteo[i]['pressure'] = str(round(bufPress, 1))       
         except:
             continue
+    # print(lisDictMeteo)
     return lisDictMeteo
+
+
 lisDictMeteo = DataPressure()
+tick = time.strftime("%Y-%m-%d/%H:%M:%S", time.localtime(time.time()))
+# print(tick)
+
+# for i in list(lisDictMeteo.keys()):
+#     print(lisDictMeteo[i])
+#     print('\n')
+
+def baseCycle():
+    b_time = time.time()
+    e_time = time.time()
+    while True:
+        tick = time.strftime("%Y-%m-%d/%H:%M:%S", time.localtime(time.time()))
+        e_time = time.time()
+        if (e_time - b_time)//2 == 10:
+            print(tick)
+            b_time = e_time
+            lisDictMeteo = DataPressure()
+            break
+    return lisDictMeteo
+
+
+
